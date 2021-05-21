50b6e2ac24f648c4dea5ac0b8d8b921f6459b022
Ward Cunningham <ward@c2.com>
2021-05-21 15:00:03 -0700

    batch suite runner script

---
+++ samples/headless/suite.sh
@@ -0,0 +1,6 @@
+# Run test suite and publish Test Log
+# Usage: sh suite.sh
+
+deno run --allow-net --allow-read=. --reload testctl.js > test.log
+deno run --allow-read=. --allow-write=. --reload itemize.js
+jq '.["test-log"]' test-log.json | ssh asia 'cat > .wiki/small.fed.wiki/pages/test-log'
